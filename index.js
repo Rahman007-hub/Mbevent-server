2c9a64a0c5b846ee250a0d220891a835bc61c595
Rahman007-hub <rahmanshonibare@gmail.com>
2024-11-28 12:35:22 +0100

    added cors

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 4000;
 const userRouter = require("./routes/userRouter");
+const cors = require('cors')
 
 //middlewares
 app.use(express.json());
+app.use(cors())
 
 //routes
 app.get("/", (req, res) => {
